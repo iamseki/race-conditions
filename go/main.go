ddcad1bd570d0687c2838a3d952c958661b17a7b
iamseki <chrisyuri_19@hotmail.com>
2023-06-17 12:05:47 -0300

    feat: adds channel example of concurrently writes in go

--- go/main.go
+++ go/main.go
@@ -35,10 +35,31 @@ func countSameMemoryAddressVariableWithMutex() (total int) {
 
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
+		// m.Lock() this can be done but not works at all, because this context has no iteraction with shared resource (variable total)
 		go count(&wg, &m)
+		// m.Unlock()
 	}
 	wg.Wait()
 
+	return total
+}
+
+func countSameMemoryAddressVariableWithChannel() (total int) {
+	wg := sync.WaitGroup{}
+	c := make(chan bool, 1) // forces channel to have a buffer of one byte size, this will block other goroutines to writes it when buffer is already full
+
+	count := func(wg *sync.WaitGroup, c chan bool) {
+		c <- true // others go routine will stuck here untill the value is released by "<-c"
+		total++
+		<-c
+		wg.Done()
+	}
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go count(&wg, c)
+	}
+	wg.Wait()
 	return total
 }
 
@@ -66,5 +87,17 @@ func main() {
 	fmt.Println(`----------------------------------------
 	now mutex were using in concurrently count implementation, there is no way of goroutines access the same address at the same time
 	so the result is always 1000 as expected.
+----------------------------------------`)
+
+	fmt.Println("******** concurrently count with channel ********")
+	r = countSameMemoryAddressVariableWithChannel()
+	fmt.Println("first call result with channel: ", r)
+
+	r = countSameMemoryAddressVariableWithChannel()
+	fmt.Println("second call result with channel: ", r)
+
+	fmt.Println(`----------------------------------------
+	now channel were using in concurrently count implementation, since the channel has a buffer size of 1 byte there is no way
+	of goroutines writes on the same channel if it is full, so this will syncronize the execution. 
 ----------------------------------------`)
 }
